8201a4eac107aae78a5a37e521d1cc839e67a41a
Saša Barišić <cartman300@net.hr>
2015-10-28 22:53:45 +0100

    Added project files

---
+++ qagamex86/Syscalls.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using RGiesecke.DllExport;
+
+namespace qagamex86 {
+	public class Syscalls {
+		[DllExport("dllEntry", CallingConvention = CallingConvention.Cdecl)]
+		public static void dllEntry(IntPtr Ret, IntPtr SyscallTable, IntPtr C) {
+			
+		}
+	}
+}
